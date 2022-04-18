32dd3ac7b435d2acc826e4f9b2267a47f609f020
Otieno100 <brian.masafu@student.moringaschool.com>
2022-04-18 20:15:48 +0300

    added a method to take the Api key in the service component

--- src/app/git-service.service.ts
+++ src/app/git-service.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { GitSearch } from './git-search';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitServiceService {
+gitSearch:string = 'Otieno100'
 
-  constructor() { }
+  GitSearch() {
+    throw new Error('Method not implemented.');
+  }
+
+  
+  constructor(private http : HttpClient) {
+    
+  }
+
+
+
+  getUserInfo(){
+
+    return this.http.get<any>('https://api.github.com/users/' + this.gitSearch) 
+  }
+  changeUserName(userTypo){
+    this.gitSearch = userTypo;
+    
+  }
+  getRepos(){
+    return this.http.get<any>('https://api.github.com/users/' + this.gitSearch + '/repos') 
+  }
+
+
 }
