70b28c7451e01a6de8503d55b63561f75245b076
Otieno100 <brian.masafu@student.moringaschool.com>
2022-04-18 13:04:56 +0300

    added method and constructor to initialize repositories

--- src/app/git-search.ts
+++ src/app/git-search.ts
@@ -1,6 +1,5 @@
-export interface GitSearch {
-name : string;
-description: string;
+export class GitSearch {
+constructor(public gitName :string, public location: string ,public obtainInfo:string, public repository:number, public imageSource:string){}
 
     
 }
